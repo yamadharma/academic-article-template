9fd100ce9fe4ea07d8aa00863f577fe4e88c2801
Dmitry S. Kulyabov <yamadharma@gmail.com>
2023-12-15 17:41:29 +0300

    fix(author): update authors information

---
+++ two-lang/auto/20author_15_fedorov.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook
+ "20author_15_fedorov"
+ (lambda ()
+   (LaTeX-add-environments
+    '("collaboration" LaTeX-env-args ["argument"] 0)))
+ :latex)
+
---
+++ two-lang/auto/20author_34_stepa.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook
+ "20author_34_stepa"
+ (lambda ()
+   (LaTeX-add-environments
+    '("collaboration" LaTeX-env-args ["argument"] 0)))
+ :latex)
+
